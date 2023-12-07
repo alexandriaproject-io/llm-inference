b6e4030dc4dda821d36d3a4c969a3dc3dabf35ef
NiftyliuS <niftylius@gmail.com>
2023-12-07 22:44:13 +0200

    Added basic configurations that will be required Added logger

---
+++ config.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# Rest API server configuration
+SERVER_HOST = os.getenv("SERVER_HOST", "127.0.0.1")
+SERVER_PORT = int(os.getenv("SERVER_PORT", "5050"))
+
+# Model inference configuration
+MODEL_PATH = os.getenv("MODEL_PATH")
+ENABLE_CUDA = os.getenv("ENABLE_CUDA", "true").lower() == 'true'
+ENABLE_USE_CACHE = os.getenv("ENABLE_USE_CACHE", "true").lower() == 'true'
+MODEL_SEED = int(os.getenv("MODEL_SEED", "42"))
+
+# Token generation configuration
+MODEL_DEFAULT_NUM_BEAMS = int(os.getenv("MODEL_DEFAULT_NUM_BEAMS", "1"))
+MODEL_DEFAULT_DO_SAMPLE = os.getenv("MODEL_DEFAULT_DO_SAMPLE", "False").lower() == 'true'
+MODEL_DEFAULT_TEMPERATURE = float(os.getenv("MODEL_DEFAULT_TEMPERATURE", "1"))
+MODEL_DEFAULT_TOP_P = float(os.getenv("MODEL_DEFAULT_TOP_P", "1"))
+MODEL_DEFAULT_TOP_K = int(os.getenv("MODEL_DEFAULT_TOP_K", "50"))
+
+# Token generation penalties and limitations
+MODEL_DEFAULT_MAX_NEW_TOKENS = int(os.getenv("MODEL_DEFAULT_MAX_NEW_TOKENS", "4096"))
+MODEL_DEFAULT_REPETITION_PENALTY = float(os.getenv("MODEL_DEFAULT_REPETITION_PENALTY", "1"))
+MODEL_DEFAULT_LENGTH_PENALTY = float(os.getenv("MODEL_DEFAULT_LENGTH_PENALTY", "1"))
+
