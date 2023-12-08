65753ea8ea81e22654a527d925b714058ba8f6b1
NiftyliuS <niftylius@gmail.com>
2023-12-09 01:30:11 +0200

    Work in progress: Added generation variants including performance tests. Results with 731 tokens on 4090 using bf16:
    1 token at a time 23.658155800003442 seconds - 30.90 tokens / sec
    10 tokens at a time: 20.62435379999806 seconds - 35.44 tokens / sec
    1000 tokens at a time 20.301898499994422 seconds - 36.00 tokens / sec
    New full pass 20.297040699995705 seconds - 36.01 tokens / sec
    Old full pass: 20.334975199992186 seconds - 35.95 tokens / sec
    
    1% performance loss between 10 tokens at a time and full in-gpu generation
    16.5% performance loss between 1 token at a time and full in-gpu generation
    
    Results are consistent between runs

--- llm_model_handler.py
+++ llm_model_handler.py
@@ -35,7 +35,6 @@ class LLMModel:
         self.model = AutoModelForCausalLM.from_pretrained(
             self.model_path,
             return_dict=True,
-            load_in_8bit=self.isQuantized,
             load_in_8bit=self.is8bitQuantized,
             device_map=device_map,
             torch_dtype=self.model_type,
@@ -54,3 +53,77 @@ class LLMModel:
         self.model.eval()
         self.isReady = True
         log.info(f"Model is ready to go.")
+
+    def decode_outputs(self, outputs):
+        return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+    def tokenize_prompt(self, prompt):
+        input_ids = self.tokenizer.encode(prompt, add_special_tokens=True)
+        input_tensor = torch.tensor(input_ids)
+
+        attention_mask = (input_tensor != self.tokenizer.pad_token_id).int().unsqueeze(0).to(self.device)
+        tokens = torch.tensor(input_ids).long()
+        tokens = tokens.unsqueeze(0).to(self.device)
+        return tokens, attention_mask, input_ids
+
+    def generate(self, tokens, attention_mask, config):
+        with torch.no_grad():
+            outputs = self.model.generate(
+                tokens,
+                attention_mask=attention_mask,
+                pad_token_id=self.tokenizer.pad_token_id,
+                eos_token_id=self.tokenizer.eos_token_id,
+                use_cache=self.config["ENABLE_USE_CACHE"],
+                num_beams=config.get("num_beams", self.config["MODEL_DEFAULT_NUM_BEAMS"]),
+                do_sample=config.get("do_sample", self.config["MODEL_DEFAULT_DO_SAMPLE"]),
+                temperature=config.get("temperature", self.config["MODEL_DEFAULT_TEMPERATURE"]),
+                top_p=config.get("top_p", self.config["MODEL_DEFAULT_TOP_P"]),
+                top_k=config.get("top_k", self.config["MODEL_DEFAULT_TOP_K"]),
+                max_new_tokens=config.get("max_new_tokens", self.config["MODEL_DEFAULT_MAX_NEW_TOKENS"]),
+                repetition_penalty=config.get("repetition_penalty", self.config["MODEL_DEFAULT_REPETITION_PENALTY"]),
+                length_penalty=config.get("length_penalty", self.config["MODEL_DEFAULT_LENGTH_PENALTY"]),
+            )
+        return outputs
+
+    def generate_cache(self, input_ids, attention_mask, past_key_values, config):
+        with torch.no_grad():
+            # Generate sequences with updated past_key_values
+            model_output = self.model.generate(
+                input_ids=input_ids,
+                attention_mask=attention_mask,
+                past_key_values=past_key_values,
+                use_cache=True,  # Ensure caching is enabled
+                return_dict_in_generate=True,  # Return a dictionary containing past_key_values
+                pad_token_id=self.tokenizer.pad_token_id,
+                eos_token_id=self.tokenizer.eos_token_id,
+                num_beams=config.get("num_beams", self.config["MODEL_DEFAULT_NUM_BEAMS"]),
+                do_sample=config.get("do_sample", self.config["MODEL_DEFAULT_DO_SAMPLE"]),
+                temperature=config.get("temperature", self.config["MODEL_DEFAULT_TEMPERATURE"]),
+                top_p=config.get("top_p", self.config["MODEL_DEFAULT_TOP_P"]),
+                top_k=config.get("top_k", self.config["MODEL_DEFAULT_TOP_K"]),
+                max_new_tokens=config.get("max_new_tokens", self.config["MODEL_DEFAULT_MAX_NEW_TOKENS"]),
+                repetition_penalty=config.get("repetition_penalty", self.config["MODEL_DEFAULT_REPETITION_PENALTY"]),
+                length_penalty=config.get("length_penalty", self.config["MODEL_DEFAULT_LENGTH_PENALTY"]),
+            )
+
+        # Extract sequences and updated past_key_values
+        sequences = model_output.sequences
+        new_past_key_values = model_output.past_key_values if 'past_key_values' in model_output else None
+
+        return sequences, new_past_key_values
+
+    def generate_full(self, prompt, config):
+        (tokens, attention_mask, input_ids) = self.tokenize_prompt(prompt)
+
+        outputs = self.generate(tokens, attention_mask, config)
+        output_text = self.decode_outputs(outputs)
+        response_start_index = len(prompt)
+        response = output_text[response_start_index:].strip()
+        # TODO - use tokens to cut and decode outputs instead of text cutting...
+
+        is_eos = outputs[0][-1] == self.tokenizer.eos_token_id
+
+        return response,  is_eos
+
+    def generate_stream(self, prompt, stream_callback, finish_callback):
+        (tokens, attention_mask) = self.tokenizer(prompt)
--- server.py
+++ server.py
@@ -1,18 +1,170 @@
 from flask import Flask, request, jsonify
 from llm_model_handler import LLMModel
 import config
+import time
+import torch
 
 model_config = {
     "ENABLE_CUDA": config.ENABLE_CUDA,
     "LOAD_IN_8BIT": config.LOAD_IN_8BIT,
     "LOW_CPU_MEM_USAGE": config.LOW_CPU_MEM_USAGE,
     "TARGET_GPU_INDEX": config.TARGET_GPU_INDEX,
-    "MODEL_SEED": config.MODEL_SEED
+    "MODEL_SEED": config.MODEL_SEED,
+    "ENABLE_USE_CACHE": config.ENABLE_USE_CACHE,
+    "MODEL_DEFAULT_NUM_BEAMS": config.MODEL_DEFAULT_NUM_BEAMS,
+    "MODEL_DEFAULT_DO_SAMPLE": config.MODEL_DEFAULT_DO_SAMPLE,
+    "MODEL_DEFAULT_TEMPERATURE": config.MODEL_DEFAULT_TEMPERATURE,
+    "MODEL_DEFAULT_TOP_P": config.MODEL_DEFAULT_TOP_P,
+    "MODEL_DEFAULT_TOP_K": config.MODEL_DEFAULT_TOP_K,
+    "MODEL_DEFAULT_MAX_NEW_TOKENS": config.MODEL_DEFAULT_MAX_NEW_TOKENS,
+    "MODEL_DEFAULT_REPETITION_PENALTY": config.MODEL_DEFAULT_REPETITION_PENALTY,
+    "MODEL_DEFAULT_LENGTH_PENALTY": config.MODEL_DEFAULT_LENGTH_PENALTY,
 }
 
 llm_model = LLMModel(config.MODEL_PATH, model_config)
 llm_model.load_model()
 llm_model.run_model()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+start_prompt = "Generate a very very long poem about 1000 cats"
+def full_gen_test():
+    tokens, attention_mask, input_ids = llm_model.tokenize_prompt(start_prompt)
+
+    start_time = time.perf_counter()
+    outputs = llm_model.generate(tokens, attention_mask, {"temperature": 0, "max_new_tokens": 1000})
+    end_time = time.perf_counter()
+    execution_time = end_time - start_time
+    print(f"New full pass {execution_time} seconds")
+
+
+def full_gen_test2():
+    start_time = time.perf_counter()
+    llm_model.generate_full(start_prompt, {"temperature": 0, "max_new_tokens": 1000})
+    end_time = time.perf_counter()
+    execution_time = end_time - start_time
+    print(f"Old full pass: {execution_time} seconds")
+
+def token_by_token_test():
+    past_key_values = None
+    generated_sequence = []
+    tokens, attention_mask, input_ids = llm_model.tokenize_prompt(start_prompt)
+
+    start_time = time.perf_counter()
+    while True:
+        # Generate output using the provided function
+        outputs, past_key_values = llm_model.generate_cache(tokens, attention_mask, past_key_values,
+                                                            {"temperature": 0, "max_new_tokens": 1})
+
+        # Extract the last token from the sequence
+        next_token_id = outputs[:, -1].item()
+        generated_sequence.append(next_token_id)
+
+        # Check for end of sequence
+        if next_token_id == llm_model.tokenizer.eos_token_id:
+            break
+
+        # Update tokens for the next iteration (append the new token)
+        tokens = torch.cat((tokens, outputs[:, -1].unsqueeze(0)), dim=1)
+        attention_mask = torch.cat((attention_mask, torch.ones((attention_mask.shape[0], 1)).to(attention_mask.device)),
+                                   dim=1)
+
+    end_time = time.perf_counter()
+    execution_time = end_time - start_time
+    print(f"1 token at a time {execution_time} seconds")
+
+
+def token_by_token_test_10():
+    past_key_values = None
+    generated_sequence = []
+    tokens, attention_mask, input_ids = llm_model.tokenize_prompt(start_prompt)
+
+    start_time = time.perf_counter()
+    while True:
+        # Generate output using the provided function
+        outputs, past_key_values = llm_model.generate_cache(tokens, attention_mask, past_key_values,
+                                                            {"temperature": 0, "max_new_tokens": 10})
+
+        new_tokens = outputs[0, -10:]  # Change here to get the last 10 tokens
+        generated_sequence.extend(new_tokens.tolist())
+
+        # Check for end of sequence in the new tokens
+        if llm_model.tokenizer.eos_token_id in new_tokens:
+            break
+
+        # Update tokens for the next iteration (append the new tokens)
+        tokens = torch.cat((tokens, new_tokens.unsqueeze(0)), dim=1)
+        # Update attention mask for the new tokens
+        attention_mask = torch.cat((attention_mask, torch.ones((1, new_tokens.shape[0])).to(attention_mask.device)), dim=1)
+
+
+    end_time = time.perf_counter()
+    execution_time = end_time - start_time
+    print(f"10 tokens at a time: {execution_time} seconds")
+
+
+def token_by_token_test_1000():
+    past_key_values = None
+    generated_sequence = []
+    tokens, attention_mask, input_ids = llm_model.tokenize_prompt(start_prompt)
+
+    start_time = time.perf_counter()
+    while True:
+        # Generate output using the provided function
+        outputs, past_key_values = llm_model.generate_cache(tokens, attention_mask, past_key_values,
+                                                            {"temperature": 0, "max_new_tokens": 1000})
+
+        # Extract the last token from the sequence
+        next_token_id = outputs[:, -1].item()
+        generated_sequence.append(next_token_id)
+
+        # Check for end of sequence
+        if next_token_id == llm_model.tokenizer.eos_token_id:
+            break
+
+        # Update tokens for the next iteration (append the new token)
+        tokens = torch.cat((tokens, outputs[:, -1].unsqueeze(0)), dim=1)
+        attention_mask = torch.cat((attention_mask, torch.ones((attention_mask.shape[0], 1)).to(attention_mask.device)),
+                                   dim=1)
+
+    end_time = time.perf_counter()
+    execution_time = end_time - start_time
+    print(f"1000 tokens at a time {execution_time} seconds")
+
+
+print("Running 1 token test generation")
+
+start_time = time.perf_counter()
+llm_model.generate_full(start_prompt, {"temperature": 0, "max_new_tokens": 50})
+end_time = time.perf_counter()
+execution_time = end_time - start_time
+print(f"Test full pass: {execution_time} seconds")
+
+print("")
+
+token_by_token_test()
+token_by_token_test_10()
+token_by_token_test_1000()
+full_gen_test()
+full_gen_test2()
+
+print("")
+
+
+
+
 
 
 app = Flask(__name__)
