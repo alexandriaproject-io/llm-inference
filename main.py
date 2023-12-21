3b36fef9a474595d49bf929c65818a4882bb63d9
NiftyliuS <niftylius@gmail.com>
2023-12-21 07:35:18 +0200

    Fixed swagger by using older version ( V3 )

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from aiohttp import web
-from src.routes.routes import set_routes
+from src.routes.routes import set_routes, set_cors, set_ui
 import src.services.model_service as model_service
 import aiohttp_swagger
 from src.config import config
 
 if __name__ == '__main__':
     model_service.init_llm_model()
-
     app = web.Application()
     set_routes(app)
-
     aiohttp_swagger.setup_swagger(
         app,
+        ui_version=3,
         swagger_url="/swagger",
         title="API documentation",
-        description=f"You can find UI demos at http://localhost:{config.SERVER_PORT}/ui"
+        description=f"<h2>You can find UI demos at <a target='_blank' style=\"font-size:0.9em\" href='/ui/'>http://localhost:{config.SERVER_PORT}/ui</a></h2>",
     )
+    set_ui(app)
+    set_cors(app)
+
     web.run_app(app, host=config.SERVER_HOST, port=config.SERVER_PORT)
--- src/routes/routes.py
+++ src/routes/routes.py
@@ -8,18 +8,25 @@ async def index(request):
 
 
 def set_routes(app):
-    cors = aiohttp_cors.setup(app)
-
     app.add_routes([
-        # Static UI hosting
-        web.get('/ui', index),
-        web.get('/ui/{tail:[^\.]*}', index),
-        web.static('/ui/', path='ui-app/build', name='static'),
         # HTTP endpoints and Web Socket
         web.post('/api/generate-one', generate_controller.generate_one),
         web.post('/api/generate-batch', generate_controller.generate_batch),
         web.get('/api/ws', ws_controller.websocket_handler),
     ])
+
+
+def set_ui(app):
+    app.add_routes([
+        # Static UI hosting
+        web.get('/ui', index),
+        web.get('/ui/{tail:[^\.]*}', index),
+        web.static('/ui/', path='ui-app/build', name='static'),
+    ])
+
+
+def set_cors(app):
+    cors = aiohttp_cors.setup(app)
 
     for route in list(app.router.routes()):
         cors.add(route, {
@@ -29,4 +36,3 @@ def set_routes(app):
                 allow_headers="*",
             )
         })
-    return
