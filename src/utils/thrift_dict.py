224bb6e163be5fac51076fd4ee0f4012c744b6ab
NiftyliuS <niftylius@gmail.com>
2024-01-10 21:58:58 +0200

    added thrift to binary and thrift read convenience functions

--- src/utils/thrift_dict.py
+++ src/utils/thrift_dict.py
@@ -1,4 +1,11 @@
-def thrift_to_dict(thrift_obj):
+from thrift.protocol import TBinaryProtocol
+from thrift.transport import TTransport
+from typing import Type, TypeVar, Any, Union
+
+T = TypeVar('T')  # Generic type variable
+
+
+def thrift_to_dict(thrift_obj) -> Union[dict, list]:
     if isinstance(thrift_obj, list):
         return [thrift_to_dict(item) for item in thrift_obj]
     elif hasattr(thrift_obj, "__dict__"):
@@ -6,3 +13,16 @@ def thrift_to_dict(thrift_obj):
     else:
         return thrift_obj
 
+
+def thrift_to_binary(thrift_obj):
+    transport = TTransport.TMemoryBuffer()
+    protocol = TBinaryProtocol.TBinaryProtocol(transport)
+    thrift_obj.write(protocol)
+    return transport.getvalue()
+
+
+def thrift_read(binary_data: Any, thrift_class: Type[T]) -> T:
+    transport = TTransport.TMemoryBuffer(binary_data)
+    record = thrift_class()
+    record.read(TBinaryProtocol.TBinaryProtocol(transport))
+    return record
